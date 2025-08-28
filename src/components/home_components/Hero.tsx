173c8143c03c78444aeba771235f363fa6ea862f
Evan Phillips <evanap003300@gmail.com>
2025-08-27 23:13:49 -0400

    Updated hero

--- src/components/home_components/Hero.tsx
+++ src/components/home_components/Hero.tsx
@@ -2,7 +2,7 @@
 export const Hero = () => {
     return (
         <section className="bg-gray-50 text-center">
-            <div className="container mx-auto px-38 py-24">
+            <div className="container mx-auto px-6 md:px-12 lg:px-24 py-24">
                 <h1 className="text-4xl md:text-6xl font-extrabold text-gray-900 leading-tight">
                     Personalized Nutrition Coaching to Help You{' '}
                     <span className="text-blue-600">Lose Fat</span>,{' '}
@@ -17,9 +17,9 @@ export const Hero = () => {
                 <div className="mt-10">
                     <a
                         href="https://docs.google.com/forms/d/e/1FAIpQLSfY9aLJG3k1sotvGkUoS4EBQXqrdCU1qLUjXg1aTdwFQL2lHA/viewform?usp=header"
-                        target="_blank" 
+                        target="_blank"
                         rel="noopener noreferrer"
-                        className="bg-blue-600 text-white font-bold py-4 px-10 rounded-lg text-lg shadow-lg hover:bg-blue-700 transition-all duration-300 transform hover:scale-105"
+                        className="bg-blue-600 text-white font-bold py-3 px-8 md:py-4 md:px-10 rounded-lg text-lg shadow-lg hover:bg-blue-700 transition-all duration-300 transform hover:scale-105"
                     >
                         Apply for Coaching
                     </a>
